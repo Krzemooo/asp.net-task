95e2c49f062171a95cce5d7b0de2d568fd93923a
Dominik Krzemiński <dominik.krzeminski@gmail.com>
2022-02-12 20:43:09 +0100

    WIP

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -5,11 +5,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using LibApp_Gr3.Models;
 using LibApp_Gr3.ViewModels;
+using LibApp_Gr3.Services;
 
 namespace LibApp_Gr3.Controllers
 {
     public class CustomersController : Controller
     {
+        protected CustomerService CustomerService { get; }
+        public CustomersController(CustomerService customerService)
+        {
+            CustomerService = customerService;
+        }
         public ViewResult Index()
         {
             return View();
@@ -17,8 +23,22 @@ namespace LibApp_Gr3.Controllers
 
         public IActionResult Details(int id)
         {
-            return View(id);
+            var _entity = CustomerService.GetItem(id);
+
+            return View(_entity);
         }
 
+        public IActionResult Form(int? id)
+        {
+            if(id.HasValue)
+            {
+                var _entity = CustomerService.GetItem(id.Value);
+                return View(_entity);
+            }
+            else
+            {
+                return View(null);
+            }
+        }
     }
 }
